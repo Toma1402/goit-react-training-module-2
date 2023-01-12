f6fc473209874497ca1511f2979caaccd3226a46
Toma1402 <toma.k1402@gmail.com>
2023-01-12 16:57:28 +0200

    color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,20 @@
 import { Counter } from './Counter/Counter';
 import { Dropdown } from './Dropdown/Dropdown';
+import { ColorPicker } from './ColorPicker/ColorPicker';
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#008000' },
+  { label: 'blue', color: '#0000FF' },
+  { label: 'grey', color: '#A9A9A9' },
+  { label: 'pink', color: '#FFC0CB' },
+  { label: 'indigo', color: '#4B0082' },
+];
 export const App = () => {
   return (
     <>
       <Counter initialValue={0} />
       <Dropdown />
+      <ColorPicker options={colorPickerOptions} />
     </>
   );
 };
---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './ColorPicker.css';
+export class ColorPicker extends Component {
+  state = {
+    activeOptionIdx: 0,
+  };
+  render() {
+    return (
+      <div className="ColorPicker">
+        <h2 className="ColorPicker__title">Color Picker</h2>
+        <div>
+          {this.props.options.map(({ label, color }, index) => {
+            const optionClasses = ['ColorPicker__option'];
+
+            if (index === this.state.activeOptionIdx) {
+              optionClasses.push('ColorPicker__option--active');
+            }
+
+            return (
+              <button
+                key={label}
+                className={optionClasses.join(' ')}
+                style={{
+                  backgroundColor: color,
+                }}
+              ></button>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
